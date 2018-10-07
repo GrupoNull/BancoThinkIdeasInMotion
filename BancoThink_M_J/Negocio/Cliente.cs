5f82b221c2e80d65750af244757454c9e9624ae7
marcuxo <marcourrutiam@gmail.com>
2018-10-07 16:58:04 -0300

    Clases del proyecto

---
+++ BancoThink_M_J/Negocio/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoThink_M_J.Negocio
+{
+    class Cliente
+    {
+        private string rut, nombre, apellidoPaterno, apellidoMaterno,
+            direccionParticular, direccionLaboral, telefono, fechaIngreso;
+        private int sueldoCliente;
+
+        public Cliente()
+        {
+
+        }
+    }
+}
---
+++ BancoThink_M_J/Negocio/Credito.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoThink_M_J.Negocio
+{
+    class Credito
+    {
+        private int nuemroCredito, montoCredito, nCuotas;
+        private string fechaAutorizacion, fechaPosiblePago;
+
+        public Credito()
+        {
+
+        }
+
+
+    }
+}
